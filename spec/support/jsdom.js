a5a0b2aa3487b503c57113f5a3c61d405255f0cf
Dana Scheider <dana.scheider@gmail.com>
2015-03-16 12:58:10 -0700

    Goodbye JSDOM, hello nightwatch

--- spec/support/jsdom.js
+++
@@ -1,7 +0,0 @@
-var jsdom = require('jsdom').jsdom;
-var fs    = require('fs');
-
-var html = fs.readFileSync(process.cwd() + '/spec/support/document.html')
-
-global.document = jsdom('<!DOCTYPE html><html><head></head><body></body></html>');
-global.window   = document.defaultView;
--- spec/testToolSpec.js
+++ spec/testToolSpec.js
@@ -1,22 +1,17 @@
-require(process.cwd() + '/spec/support/jsdom.js');
-var matchers = require('jasmine-jquery-matchers');
+require('nightwatch');
 
-var View = require(process.cwd() + '/example/view1.js');
-
-describe('View 1', function() {
-  var view;
-
-  beforeEach(function() {
-    jasmine.addMatchers(matchers);
-    view = new View();
-    view.render();
-  });
-
-  it('displays objects with the `visible` class', function() {
-    expect(view.$('.visible')).toBeVisible();
-  });
-
-  it('hides objects with the `hidden` class', function() {
-    expect(view.$('.hidden')).not.toBeVisible();
-  });
-});
+module.exports = {
+  "Demo test Google" : function(client) {
+    client
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .assert.title('Google')
+      .assert.visible('input[type=text]')
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('#main', 'The Night Watch')
+      .end();
+  }
+}
